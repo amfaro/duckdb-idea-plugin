ad7dd9a178ee9d83636c3d36a38f474067d17825
John Allen <john@threedogconsulting.com>
2026-05-07 13:43:39 -0400

    feat: add DuckDB circle logo to SQL Dialects picker

--- src/main/kotlin/com/amfaro/duckdb/dbms/DuckDbDbms.kt
+++ src/main/kotlin/com/amfaro/duckdb/dbms/DuckDbDbms.kt
@@ -1,11 +1,12 @@
 package com.amfaro.duckdb.dbms
 
 import com.intellij.database.Dbms
-import com.intellij.util.ui.EmptyIcon
+import com.intellij.openapi.util.IconLoader
 
-/** Registers a custom Dbms constant so the platform can group DuckDB dialects, data sources, and consoles by it. */
 object DuckDbDbms {
 
     @JvmField
-    val INSTANCE: Dbms = Dbms.create("DUCKDB", "DuckDB") { EmptyIcon.ICON_16 }
+    val INSTANCE: Dbms = Dbms.create("DUCKDB", "DuckDB") {
+        IconLoader.getIcon("/icons/duckdb.svg", DuckDbDbms::class.java)
+    }
 }
